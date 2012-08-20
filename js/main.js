7fc65514a82e0d7452cc939b6ee9ee8a954e5935
Edmund Mai <edmundmai@gmail.com>
2012-08-20 17:15:17 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+
+	
+//code for short paragraph to appear when mouse hovers over picture 
+//---------------------------------
+
+	function mouseOn(){
+		var height = $(this).find('#para').height() + 25;
+		$(this).find('#para').animate({bottom: height}, 400);
+	};
+
+	function mouseOff(){
+		$(this).find('#para').animate({bottom: 0}, 400);
+	};
+
+	$('.bottom img').hover(mouseOn, mouseOff);
+
+
+	$('.pic:not(:first)').hide();
+	$currentBox = $('.pic');
+
+//---------------------------------
+
+
+//code for slideshow to automatically slide after every few seconds, as well as navigating through the images through the slideshow buttons below the slideshow
+//---------------------------------
+
+	$('ul li:eq(0)').addClass('selected');
+	
+	$('ul li').click(function(){
+		$('.image_frame img').fadeOut(300);
+		$('ul li').removeClass('selected');
+		$(this).addClass('selected');
+		var listPos = $(this).index();
+		$('.image_frame img').eq(listPos).fadeIn(300);
+		if (listPos < imgChild.length - 1){
+			current = listPos + 1;
+		} else {
+			current = 0;
+		}
+	});
+
+	var hey = $('.image_frame img').size();
+	var current = 1	
+	var imgChild = $('.image_frame').children()
+
+
+	function killMeNow(){
+		$('.image_frame img').fadeOut(300);
+		$('.image_frame img').eq(current).fadeIn(300);
+		$('li').removeClass('selected');
+		$('li').eq(current).addClass('selected');
+		current += 1;
+		if (current >= imgChild.length) {
+			current = 0;
+		};
+		setTimeout(killMeNow, 10000);
+	};
+
+	setTimeout(killMeNow, 10000);
+
+//---------------------------------
+
+});
